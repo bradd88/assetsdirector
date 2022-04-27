e864caee9858832601cd823e9c10fb5f031d36af
Bradley Duke <bradduke88@gmail.com>
2022-04-27 16:57:46 -0500

    Code cleanup for MySql class. Added support for NULL values in where/set clauses. Made return types more consistent. Added some error checking. Minor performance improvement.

--- model/service/mySql.php
+++ model/service/mySql.php
@@ -25,7 +25,7 @@ class MySql
     {
         $this->connection = new mysqli($this->dbSettings->host, $this->dbSettings->user, $this->dbSettings->password, $this->dbSettings->database);
         if ($this->connection->connect_error) {
-            die("Connection failed: " . $this->connection->connect_error);
+            throw new Exception("Connection failed: " . $this->connection->connect_error);
         }
     }
 
@@ -35,119 +35,97 @@ class MySql
         $this->connection->close();
     }
 
-    /** Run a supplied query and return the appropriate information from the results.
-     * 
-     * @return mixed
-     * INSERT returns the ID of the inserted row.
-     * SELECT returns an array of objects representing table rows.
-     * UPDATE/DELTE/TRUNCATE returns the number of affected rows.
-     * If there is an errror with the query, a string containing error data is returned instead.
-     */
-    private function query(string $queryType, string $queryString): mixed
+    /** Run a query on the database. */
+    private function query(string $queryString): mysqli
     {
-        // Perform the requested query and return the relevant information from the response.
         $timerStart = microtime(TRUE);
-        $queryResponse = $this->connection->query($queryString);
+        $queryResponse = $this->connection->real_query($queryString);
         $timerStop = microtime(TRUE);
         $executeTime = bcsub("$timerStop", "$timerStart", 5);
         $this->log->save('database_queries', 'Query took ' . $executeTime . ' seconds: ' . $queryString);
         if ($queryResponse === FALSE) {
-            return 'Query: ' . $queryString . '\nError: ' . $this->connection->error;
-        } else {
-            if ($queryType === 'INSERT') {
-                return $this->connection->insert_id;
-            } elseif ($queryType === 'SELECT') {
-                return json_decode(json_encode($queryResponse->fetch_all(MYSQLI_ASSOC)));
-            } elseif ($queryType === 'UPDATE' || $queryType === 'DELETE' || $queryType === 'TRUNCATE') {
-                return $this->connection->affected_rows;
-            }
+            throw new Exception('Query: ' . $queryString . '\nError: ' . $this->connection->error);
         }
+        return $this->connection;
     }
 
-    /** Execute a prepared statement string, and record execution times in the logs.
-     * 
-     * @return mysqli_stmt|false Returns the statement object, or bool FALSE if there is an error.
-     */
-    private function preparedStatement(string $queryString, array $bindParams): mixed
+    /** Execute a prepared statement and returns the mysqli_stmt object. */
+    private function preparedStatement(string $queryString, array $bindParams): mysqli_stmt
     {
+        $timerStart = microtime(TRUE);
         $preparedStatement = $this->connection->prepare($queryString);
         $preparedStatement->bind_param(...$bindParams);
-        $timerStart = microtime(TRUE);
         $preparedStatement->execute();
+        $preparedStatement->store_result();
         $timerStop = microtime(TRUE);
         $executeTime = bcsub("$timerStop", "$timerStart", 5);
-        $preparedStatement->store_result();
         $this->log->save('database_queries', 'Query took ' . $executeTime . ' seconds: ' . $queryString);
+        if ($preparedStatement === FALSE) {
+            throw new Exception('Query: ' . $queryString . '\nError: ' . $this->connection->error);
+        }
         return $preparedStatement;
     }
 
-    /** Create a new user in the database using a prepared statement. */
-    public function createUser(string $username, string $password): string
+    /** Attempt to create a new user entry in the database. */
+    public function createUser(string $username, string $password): bool
     {
-        // Check if the username already exists.
         $queryString = 'SELECT `account_id` FROM `accounts` WHERE `username` = ?';
         $bindParams = array('s', $username);
         $checkUsername = $this->preparedStatement($queryString, $bindParams);
-        
-        // Create the new account if the username isn't taken.
         if ($checkUsername->num_rows === 0) {
             $hash = password_hash($password, PASSWORD_DEFAULT);
             $created = time();
             $queryString = 'INSERT INTO `accounts` (`username`, `hash`, `created`) VALUES (?, ?, ?)';
             $bindParams = array('ssi', $username, $hash, $created);
             $this->preparedStatement($queryString, $bindParams);
-            return 'Account created.';
-        } else {
-            return 'Account already exists.';
-        }
-    }
-
-    /** Verify that provided user credentials are good.
-     * 
-     * @return int|false Returns the verified account ID if username/password are good. Returns FALSE if the username/password are bad.
-    */
-    public function verifyLogin(string $username, string $password): mixed
-    {
-        // Retrieve the account information.
-        $queryString = 'SELECT `account_id`, `hash` FROM `accounts` WHERE `username` = ?';
-        $bindParams = array('s', $username);
-        $verifyLogin = $this->preparedStatement($queryString, $bindParams);
-
-        // If an account matching the username exists, then store the account ID and password hash.
-        if ($verifyLogin->num_rows === 1) {
-            $account_id = '';
-            $hash = '';
-            $verifyLogin->bind_result($account_id, $hash);
-            $verifyLogin->fetch();
-            $verifyStatus = (password_verify($password, $hash) === TRUE) ? $account_id : FALSE;
-            return $verifyStatus;
+            return TRUE;
         } else {
             return FALSE;
         }
     }
 
+    /** Verify that provided user credentials are good and return the account ID. */
+    public function verifyLogin(string $username, string $password): int|false
+    {
+        $queryString = 'SELECT `account_id`, `hash` FROM `accounts` WHERE `username` = ?';
+        $bindParams = array('s', $username);
+        $account = $this->preparedStatement($queryString, $bindParams);
+        if ($account->num_rows === 1) {
+            $account->bind_result($account_id, $hash);
+            $account->fetch();
+            if (password_verify($password, $hash)) {
+                return $account_id;
+            }
+        }
+        return FALSE;
+    }
+
     /** Convert an array into a Columns/Values string for use in the WHERE, WHERE/BETWEEN, SET, or INSERT INTO/VALUES portions of a query. */
     private function arrayToQueryString(array $array, string $type): string
     {
-
         $concatenator = ($type === 'where') ? ' AND ' : ', ';
         $string = '';
-        foreach ($array as $key => $value) {
-            if ($type == 'where') {
-                if (!is_array($value)) {
-                    $content = '`' . $key . '` = \'' . $value . '\'';
+        foreach ($array as $column => $value) {
+            if ($type === 'where') {
+                if (is_array($value)) {
+                    $string .= '`' . $column . '` BETWEEN \'' . $value[0] . '\' AND \'' . $value[1] . '\'';
+                } elseif (is_null($value)) {
+                    $string .= '`' . $column . '` IS NULL';
                 } else {
-                    $content = '`' . $key . '` BETWEEN \'' . $value[0] . '\' AND \'' . $value[1] . '\'';
+                    $string .= '`' . $column . '` = \'' . $value . '\'';
                 }
-            }elseif ($type == 'set') {
-                $content = '`' . $key . '` = \'' . $value . '\'';
-            } elseif ($type == 'insertColumn') {
-                $content = '`' . $key . '`';
-            } elseif ($type == 'insertValue') {
-                $content = '\'' . $value . '\'';
+            } elseif ($type === 'set') {
+                if (is_null($value)) {
+                    $string .= '`' . $column . '` = NULL';
+                } else {
+                    $string .= '`' . $column . '` = \'' . $value . '\'';
+                }
+            } elseif ($type === 'insertColumn') {
+                $string .= '`' . $column . '`';
+            } elseif ($type === 'insertValue') {
+                $string .= '\'' . $value . '\'';
             }
-            $string .= $content;
-            if ($key !== array_key_last($array)) {
+            if ($column !== array_key_last($array)) {
                 $string .= $concatenator;
             }
         }
@@ -155,16 +133,16 @@ class MySql
     }
 
     /** Create rows in the database using Key/Value pairs from $insertArray. */
-    public function create(string $tableName, array $insertArray): mixed
+    public function create(string $tableName, array $insertArray): string
     {
         $columnString = $this->arrayToQueryString($insertArray, 'insertColumn');
         $valueString = $this->arrayToQueryString($insertArray, 'insertValue');
         $queryString = 'INSERT INTO `' . $tableName . '` (' . $columnString . ') VALUES (' . $valueString . ') ON DUPLICATE KEY UPDATE `id`=`id`';
-        return $this->query('INSERT', $queryString);
+        return $this->query($queryString)->insert_id;
     }
 
-    /** Read rows from the database using Key/Value pairs from $whereArray. */
-    public function read(string $tableName, array $whereArray = NULL, string $orderBy = NULL): mixed
+    /** Read from the database and return the results an array of objects representing the rows. */
+    public function read(string $tableName, array $whereArray = NULL, string $orderBy = NULL): array
     {
         $queryString = 'SELECT * FROM `' . $tableName . '`';
         if ($whereArray !== NULL) {
@@ -174,31 +152,35 @@ class MySql
         if ($orderBy !== NULL) {
             $queryString .= ' ORDER BY ' . $orderBy;
         }
-        return $this->query('SELECT', $queryString);
+        $results = $this->query($queryString)->store_result()->fetch_all(MYSQLI_ASSOC);
+        foreach ($results as &$result) {
+            $result = (object) $result;
+        }
+        return $results;
     }
 
     /** Update rows in the database using Key/Value pairs from $whereArray and $setArray. */
-    public function update(string $tableName, array $whereArray, array $setArray): mixed
+    public function update(string $tableName, array $whereArray, array $setArray): int
     {
         $whereString = $this->arrayToQueryString($whereArray, 'where');
         $setString = $this->arrayToQueryString($setArray, 'set');
         $queryString = 'UPDATE `' . $tableName . '` SET ' . $setString . ' WHERE ' . $whereString;
-        return $this->query('UPDATE', $queryString);
+        return $this->query($queryString)->affected_rows;
     }
 
     /** Delete selected rows from the database using Key/Value pairs from $whereArray. */
-    public function delete(string $tableName, array $whereArray): mixed
+    public function delete(string $tableName, array $whereArray): int
     {
-        $whereString = $this->arrayToQueryString($whereArray, ' AND ');
+        $whereString = $this->arrayToQueryString($whereArray, 'where');
         $queryString = 'DELETE FROM `' . $tableName . '` WHERE ' . $whereString;
-        return $this->query('DELETE', $queryString);
+        return $this->query($queryString)->affected_rows;
     }
 
     /** Delete an entire selected table from the database. */
-    public function truncate(string $tableName): mixed
+    public function truncate(string $tableName): int
     {
         $queryString = 'TRUNCATE `' . $this->dbSettings->database . '`.`' . $tableName . '`';
-        return $this->query('TRUNCATE', $queryString);
+        return $this->query($queryString)->affected_rows;
 
     }
 
