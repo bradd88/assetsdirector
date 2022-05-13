91a2448e962148230a7cccc5ce686a2b1f56e375
Bradley Duke <bradduke88@gmail.com>
2022-05-12 20:58:08 -0500

    Added cast method for TradeFactory

--- model/logic/tradeFactory.php
+++ model/logic/tradeFactory.php
@@ -5,9 +5,26 @@ class TradeFactory
     public function __construct()
     {
     }
+
     public function create(): Trade
     {
         return new Trade();
+    }
+
+    public function cast(object $object): Trade
+    {
+        $trade = new Trade();
+        foreach ($object as $property => $value) {
+            $trade->$property = $value;
+        }
+        $reflection = new ReflectionClass('Trade');
+        foreach ($reflection->getProperties() as $property) {
+            $propertyName = $property->getName();
+            if (!isset($trade->$propertyName)) {
+                throw new Exception('Unable to cast trade, missing property: ' . $propertyName);
+            }
+        }
+        return $trade;
     }
 }
 
