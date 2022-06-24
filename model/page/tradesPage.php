20f2c034b9cbbfb3e36e06b60c736d2137023500
Bradley Duke <bradduke88@gmail.com>
2022-06-24 11:34:36 -0500

    Re-Added trades page.

--- model/page/tradesPage.php
+++ model/page/tradesPage.php
@@ -5,27 +5,61 @@ class TradesPage extends AbstractPage
     protected View $view;
     private MySql $mySql;
     private Login $login;
+    private TradeListFactory $tradeListFactory;
+    private TradeFactory $tradeFactory;
+    private GraphFactory $graphFactory;
 
-    public function __construct(View $view, MySql $mySql, Login $login)
+    public function __construct(
+        View $view,
+        MySql $mySql,
+        Login $login,
+        TradeListFactory $tradeListFactory,
+        TradeFactory $tradeFactory,
+        GraphFactory $graphFactory
+        )
     {
         $this->view = $view;
         $this->mySql = $mySql;
         $this->login = $login;
+        $this->tradeListFactory = $tradeListFactory;
+        $this->tradeFactory = $tradeFactory;
+        $this->graphFactory = $graphFactory;
     }
 
     public function exec(): string
     {
-        $transactions = $this->mySql->read(
-            'transactions',
+        // Get the trades and add running statistics.
+        $trades = $this->mySql->read(
+            'trades',
             NULL,
             [
                 ['account_id', 'isEqual', $this->login->getAccountId()],
-                ['type', 'isEqual', 'TRADE'],
                 ['assetType', 'isEqual', 'EQUITY'],
-                ['transactionDate', 'isBetween', ['2021-01-01T00:00:00+0000', '2021-10-30T00:00:00+0000']]
+                ['stop', 'isBetween', ['2021-01-01T00:00:00+0000', '2021-02-30T00:00:00+0000']]
+            ],
+            [
+                ['start', 'ASC']
             ]
         );
-        return $this->generate('page/transactions.phtml', ['transactions' => $transactions], TRUE);
+
+        // Create the Trades list and generate statistics.
+        $tradeList = $this->tradeListFactory->create();
+        foreach ($trades as $row) {
+            $trade = $this->tradeFactory->cast($row);
+            $tradeList->addTrade($trade);
+        }
+        $tradeList->addStatistics();
+
+        // Generate the graph of trade data.
+        $graph = $this->graphFactory->create();
+        $graph->addLine('Returns', 'black', $tradeList->generateGraphData());
+        $graph->generate(25);
+
+        // Generate html.
+        $tradeTable = $this->view->get('page/trades.phtml', ['calendar' => $this->calendar, 'trades' => $tradeList->getTrades()]);
+        $graphCanvas = $this->view->get('presentation/graph.phtml', ['graph' => $graph]);
+        $content = $graphCanvas . $tradeTable;
+        return $this->generatePage($content, TRUE);
     }
 }
 
